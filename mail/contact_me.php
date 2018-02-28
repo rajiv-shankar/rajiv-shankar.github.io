8682f71a7b2caac55ecf962832ab7827d0dc12e1
Rajiv Shankar <rajiv.shankar.email@gmail.com>
2018-02-28 15:42:31 -0500

    add link to Udacity FEND page, update CONTACT ME php info

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -3,7 +3,7 @@
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
+   empty($_POST['message'])	  ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'rajiv.shankar.email@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@rajiv-shankar.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
